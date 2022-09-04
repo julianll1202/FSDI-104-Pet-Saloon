5bfbb035b629329ccf8d3fefb0b158dc14f0f4ed
julianll1202 <a20490712@itmexicali.edu.mx>
2022-09-03 18:36:52 -0700

    Display pets in a table

--- scripts/display.js
+++ scripts/display.js
@@ -1,20 +1,33 @@
 function displayPetCards(){
-    const petsDiv = document.getElementById("pets");
-    petsDiv.innerHTML=``;
+    const petsDiv = document.getElementById("pets-table");
+    petsDiv.innerHTML=`
+    <thead class="table-dark">
+        <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Gender</th>
+            <th>Breed</th>
+            <th>Service</th>
+            <th>Owner's name</th>
+            <th>Contact phone</th>
+        </tr>
+    </thead>
+    `;
     //travel the array of pets
     for(let i=0; i<petSalon.pets.length; i++){
         let pet = petSalon.pets[i];
         //create template
         let tmp=`
-        <div class="pet">
-            <p><b>Name:</b> ${pet.name}</p>
-            <p><b>Age:</b>${pet.age}</p>
-            <p><b>Gender:</b> ${pet.gender}</p>
-            <p><b>Breed:</b> ${pet.breed}</p>
-            <p><b>Service:</b> ${pet.service}</p>
-            <p><b>Owner's name:</b> ${pet.owner}</p>
-            <p><b>Contact phone:</b> ${pet.phone}</p>
-        </div>`;
+        
+        <tr class="pet">
+            <td>${pet.name}</td>
+            <td>${pet.age}</td>
+            <td>${pet.gender}</td>
+            <td>${pet.breed}</td>
+            <td>${pet.service}</td>
+            <td>${pet.owner}</td>
+            <td>${pet.phone}</td>
+        </tr>`;
         //insert template into html
         petsDiv.innerHTML+=tmp;
     }
